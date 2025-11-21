7c99435f2d692d2d61e7be315573834f17c70281
bforce54 <yoshua1alexander@gmail.com>
2025-11-21 13:33:52 -0600

    onboard project done

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px
+
+# loads CSV
+df = pd.read_csv("can_data.csv")
+
+# dynamically remove all 'Analog Input' columns where ALL values are constant
+
+
+dead_cols = [
+    col for col in df.columns
+    if "Analog Input" in col and df[col].nunique() == 1
+]
+df = df.drop(columns=dead_cols)
+
+print("Removed dead analog columns:", dead_cols)
+
+
+# plot RPM over time
+fig_rpm = px.line(df, x=df.columns[0], y="RPM", title="RPM Over Time")
+fig_rpm.show()
+
+# plot TPS over time
+fig_tps = px.line(df, x=df.columns[0], y="TPS", title="TPS Over Time")
+fig_tps.show()
+
